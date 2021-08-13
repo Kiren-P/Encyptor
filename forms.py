dc778ef7eb2d8c63841685d5918c90afe32905e5
Kiren <kirenp.2004@gmail.com>
2021-08-13 14:38:02 -0300

    Removed unnessecary file

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.fields.simple import SubmitField
-from wtforms.form import Form
-from wtforms.validators import DataRequired
-
-class EncryptForm(FlaskForm):
-    new_string = StringField("Text", validators=[DataRequired()])
-    submit = SubmitField("Submit")
